7fc87dd3b93a6029245cd61d00ecef2ed3d0ea4f
Gustavo Bauke <baukegustavo@gmail.com>
2022-03-18 14:16:01 +0000

    remove código desnecessário

--- src/pages/processo-seletivo.js
+++ src/pages/processo-seletivo.js
@@ -1,30 +1,21 @@
-import React from 'react'
-import Button from '../components/Button'
-import OutlinedButton from '../components/OutlinedButton'
-import Card from '../components/Card'
-import CustomerCard from '../components/CustomerCard'
-import LabelText from '../components/LabelText'
-import Layout from '../components/layout/Layout'
-import Seo from '../components/Seo'
-import SplitSection from '../components/SplitSection'
-import StatsBox from '../components/StatsBox'
-import customerData from '../data/customer-data'
-import HeroImage from '../svg/HeroImage'
-import SvgCharts from '../svg/SvgCharts'
-import { StaticImage } from 'gatsby-plugin-image'
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+
+import Layout from '../components/layout/Layout';
+import Seo from '../components/Seo';
 
 const ProcessoSeletivo = () => {
-    return (
+  return (
     <Layout>
       <section className="mt-8 py-20 lg:pb-40 lg:pt-48">
         <Seo title="Processo Seletivo" />
-        
-        <div className="container mx-auto flex flex-col items-center md:items-start">
-          <h2 className="text-3xl lg:text-5xl font-semibold">
-            Processo Seletivo 2022
-          </h2>
 
-          <div style={{ width: 'min(400px, 80vw)', height: 'min(400px, 80vw)', alignSelf: 'center' }}>
+        <div className="container mx-auto flex flex-col items-center md:items-start">
+          <h2 className="text-3xl lg:text-5xl font-semibold">Processo Seletivo 2022</h2>
+
+          <div
+            style={{ width: 'min(400px, 80vw)', height: 'min(400px, 80vw)', alignSelf: 'center' }}
+          >
             <StaticImage
               src="../../static/images/processo-seletivo.png"
               alt="Processo Seletivo Seiva Jr. 2022"
@@ -34,12 +25,11 @@ const ProcessoSeletivo = () => {
           </div>
 
           <p className="mt-4 text-md md:text-lg px-8">
-          O processo seletivo de 2022 abrirá em breve! Atente-se às nossas redes sociais para saber quando o processo abrirá 
-            e como ele funcionará!
-            <br/>
-            <br/>
-            A Seiva Jr. é uma Empresa Júnior da Universidade Federal do ABC (UFABC) que visa promover o conceito de{' '}
-            <span className="text-primary font-bold">SUSTENTABILIDADE</span> a partir da{' '}
+            O processo seletivo de 2022 abrirá em breve! Atente-se às nossas redes sociais para
+            saber quando o processo abrirá e como ele funcionará!
+            <br />
+            <br /> A Seiva Jr. é uma Empresa Júnior da Universidade Federal do ABC (UFABC) que visa
+            promover o conceito de <span className="text-primary font-bold">SUSTENTABILIDADE</span> a partir da{' '}
             <span className="text-primary font-bold">DIFUSÃO de CONHECIMENTO</span>, da{' '}
             <span className="text-primary font-bold">INOVAÇÃO</span> e de{' '}
             <span className="text-primary font-bold">CONSULTORIAS</span>.
--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -1,66 +1,49 @@
-import React from 'react'
-import Button from '../components/Button'
-import OutlinedButton from '../components/OutlinedButton'
-import Card from '../components/Card'
-import CustomerCard from '../components/CustomerCard'
-import LabelText from '../components/LabelText'
-import Layout from '../components/layout/Layout'
-import SplitSection from '../components/SplitSection'
-import StatsBox from '../components/StatsBox'
-import Seo from '../components/Seo'
-import customerData from '../data/customer-data'
-import HeroImage from '../svg/HeroImage'
-import SvgCharts from '../svg/SvgCharts'
-import { GatsbyImage } from 'gatsby-plugin-image'
+import React from 'react';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import { graphql } from 'gatsby';
+
+import Seo from '../components/Seo';
+import Layout from '../components/layout/Layout';
 
 // import membros from '../data/membros-data.json'
-import { graphql } from 'gatsby'
 
 export const pageData = graphql`
-query AboutQuery {
-  allMembrosDataJson {
-    edges {
-      node {
-        name
-        image {
-					childImageSharp {
-          	gatsbyImageData(
-							width: 120,
-              height: 120
-            )
+  query AboutQuery {
+    allMembrosDataJson {
+      edges {
+        node {
+          name
+          image {
+            childImageSharp {
+              gatsbyImageData(width: 120, height: 120)
+            }
           }
         }
       }
     }
   }
-}
-
-`
+`;
 
 const Sobre = ({ data }) => {
-  const membros = data.allMembrosDataJson.edges
-
-  console.log(membros)
+  const membros = data.allMembrosDataJson.edges;
 
   return (
     <Layout>
       <section id="sobre" className="mt-8 pt-20 lg:pb-20 lg:pt-48">
         <Seo title="Sobre Nós" />
-        
+
         <div className="container mx-auto flex flex-col items-center md:items-start">
           <h2 className="title lg:text-5xl">Sobre Nós</h2>
-
-
         </div>
       </section>
       <section id="equipe" className="mt-8 pt-20 lg:pb-20 lg:pt-48">
         <Seo title="Equipe" />
-        
+
         <div className="container mx-auto flex flex-col items-center md:items-start">
           <h2 className="title lg:text-5xl mb-4">Equipe</h2>
 
           <div className="equipe-grid">
-            {membros.map(membro => (
+            {membros.map((membro) => (
               <GatsbyImage
                 key={membro.node.name}
                 image={membro.node.image.childImageSharp.gatsbyImageData}

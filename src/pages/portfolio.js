11240a0cf5ea11be84db01a4f67d5ef1aa48940a
Gustavo Teodoro Bauke <baukegustavo@gmail.com>
2022-06-30 18:13:09 -0300

    adiciona link para portfólio e atualiza cargo dos membros

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -11,13 +11,28 @@ const Portfolio = () => {
         <Seo title="Portfólio" />
 
         <div className="container mx-auto flex flex-col items-center md:items-start">
-          <h2 className="title lg:text-5xl">O que podemos fazer por você?</h2>
-          <p className="text-xl lg:text-2xl mt-6 font-light px-8">
-            Você entra com a sua necessidade, tanto pessoal quanto empresarial e nós entramos com a
-            atitude, visão e conhecimento universitário sobre Sustentabilidade integrada em todas as
-            áreas. A Seiva Jr. pode tornar seus projetos eco-friendly ou então ajudar a desenvolver
-            uma nova ideia.
-          </p>
+          <div className="mb-8">
+            <h2 className="title lg:text-5xl">O que podemos fazer por você?</h2>
+            <p className="text-xl mt-6 font-light px-8">
+              Você entra com a sua necessidade, tanto pessoal quanto empresarial e nós entramos com
+              a atitude, visão e conhecimento universitário sobre Sustentabilidade integrada em
+              todas as áreas. A Seiva Jr. pode tornar seus projetos eco-friendly ou então ajudar a
+              desenvolver uma nova ideia.
+            </p>
+          </div>
+          <div>
+            <p className="text-xl mt-6 font-light px-8">
+              Para saber mais, baixe nosso portfólio e veja os serviços que já prestamos:
+            </p>
+            <a
+              className="text-xl mt-6 font-light px-8"
+              href="https://drive.google.com/file/d/1sAAt6j86k4UjabVBLi59CvqwKmF-gQj9/view"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Acesse nosso portfólio
+            </a>
+          </div>
         </div>
       </section>
       <Services />

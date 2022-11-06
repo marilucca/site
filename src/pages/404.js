f5eb9d553f49070275ce87f744845407e741f806
Gustavo Teodoro Bauke <baukegustavo@gmail.com>
2022-11-06 17:21:49 -0300

    arruma layout da página 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,9 +4,9 @@ import Layout from '../components/layout/Layout';
 
 const NotFound = () => (
   <Layout>
-    <div className="flex justify-center items-center">
+    <div className="section-container flex justify-center items-center">
       <h1 className="title lg:text-5xl">Página não encontrada!</h1>
-      <p>Você entrou em uma página que apenas não existe :(</p>
+      <p>Você entrou em uma página que não existe :(</p>
     </div>
   </Layout>
 );
